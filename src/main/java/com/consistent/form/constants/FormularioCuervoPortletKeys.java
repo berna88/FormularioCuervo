1d9fbd06108cfc94949b1288a79856cf66fe0ede
Bernardo <berna_halo@hotmail.com>
2019-08-21 18:14:29 -0500

    Initial commit

---
+++ src/main/java/com/consistent/form/constants/FormularioCuervoPortletKeys.java
@@ -0,0 +1,10 @@
+package com.consistent.form.constants;
+
+/**
+ * @author bernardohernandez
+ */
+public class FormularioCuervoPortletKeys {
+
+	public static final String FormularioCuervo = "formulariocuervo";
+
+}
---
+++ src/main/java/com/consistent/form/portlet/FormularioCuervoPortlet.java
@@ -0,0 +1,28 @@
+package com.consistent.form.portlet;
+
+import com.consistent.form.constants.FormularioCuervoPortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author bernardohernandez
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + FormularioCuervoPortletKeys.FormularioCuervo,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class FormularioCuervoPortlet extends MVCPortlet {
+}
